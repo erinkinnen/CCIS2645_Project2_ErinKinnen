7f448e28468acd5d2048aa70b7e3a63837d606b9
Erin Kinnen <erin.kinnen@gmail.com>
2020-01-31 12:39:56 -0600

    Initial Project 2 commit with Database

---
+++ CCIS2645_Project2_ErinKinnen/Technician.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CCIS2645_Project2_ErinKinnen
+{
+    public partial class Technician : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            txtError.Text = "Error Message";
+        }
+
+        protected void btnMain_Technician_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("MainMenu.aspx");
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CCIS2645_Project2_ErinKinnen/clsDatabase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CCIS2645_Project2_ErinKinnen
+{
+    public class clsDatabase
+    {
+    }
+}
