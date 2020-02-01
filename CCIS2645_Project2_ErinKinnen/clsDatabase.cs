2841063a4457e03cee33e2ffefea5af14d5b4b7e
Erin Kinnen <erin.kinnen@gmail.com>
2020-02-01 15:48:58 -0600

    Adding GetProducts()

--- CCIS2645_Project2_ErinKinnen/clsDatabase.cs
+++ CCIS2645_Project2_ErinKinnen/clsDatabase.cs
@@ -42,37 +42,90 @@ namespace CCIS2645_Project2_ErinKinnen
                 return cnSQL;
             }
         }
-
-    }
-    public static DataSet GetProductByID(string strProdID)
-    {
-        SqlConnection cnSQL;
-        SqlCommand cmdSQL;
-        SqlDataAdapter daSQL;
-        DataSet dsSQL = null;
-        Boolean blnErrorOccurred = false;
-        Int32 intRetCode;
-
-        if (strProdID.Trim().Length > 0)
+        public static DataSet GetProductByID(string strProdID)
         {
+            SqlConnection cnSQL;
+            SqlCommand cmdSQL;
+            SqlDataAdapter daSQL;
+            DataSet dsSQL = null;
+            Boolean blnErrorOccurred = false;
+            Int32 intRetCode;
+
+            if (strProdID.Trim().Length > 0)
+            {
+                cnSQL = AcquireConnection();
+                if (cnSQL == null)
+                {
+                    blnErrorOccurred = true;
+                }
+                else
+                {
+                    cmdSQL = new SqlCommand();
+                    cmdSQL.Connection = cnSQL;
+                    cmdSQL.CommandType = CommandType.StoredProcedure;
+                    cmdSQL.CommandText = "uspGetProductByID";
+
+                    cmdSQL.Parameters.Add(new SqlParameter("@ProductID", SqlDbType.NVarChar, 10));
+                    cmdSQL.Parameters["@ProductID"].Direction = ParameterDirection.Input;
+                    cmdSQL.Parameters["@ProductID"].Value = strProdID;
+
+                    cmdSQL.Parameters.Add(new SqlParameter("@ErrCode", SqlDbType.Int));
+                    cmdSQL.Parameters["ErrCode"].Direction = ParameterDirection.ReturnValue;
+
+                    dsSQL = new DataSet();
+                    try
+                    {
+                        daSQL = new SqlDataAdapter(cmdSQL);
+                        intRetCode = daSQL.Fill(dsSQL);
+                        daSQL.Dispose();
+                    }
+                    catch (Exception ex)
+                    {
+                        blnErrorOccurred = true;
+                        dsSQL.Dispose();
+                    }
+                    finally
+                    {
+                        cmdSQL.Parameters.Clear();
+                        cmdSQL.Dispose();
+                        cnSQL.Close();
+                        cnSQL.Dispose();
+                    }
+                }
+            }
+
+            if (blnErrorOccurred)
+            {
+                return null;
+            }
+            else
+            {
+                return dsSQL;
+            }
+        }
+        public static DataSet GetProductList()
+        {
+            SqlConnection cnSQL;
+            SqlCommand cmdSQL;
+            SqlDataAdapter daSQL;
+            DataSet dsSQL = null;
+            Boolean blnErrorOccurred = false;
+            Int32 intRetCode;
+
             cnSQL = AcquireConnection();
             if (cnSQL == null)
             {
-                blnErrorOccurred = true;
+                return null;
             }
             else
             {
                 cmdSQL = new SqlCommand();
                 cmdSQL.Connection = cnSQL;
                 cmdSQL.CommandType = CommandType.StoredProcedure;
-                cmdSQL.CommandText = "uspGetProductByID";
-
-                cmdSQL.Parameters.Add(new SqlParameter("@ProductID", SqlDbType.NVarChar, 10));
-                cmdSQL.Parameters["@ProductID"].Direction = ParameterDirection.Input;
-                cmdSQL.Parameters["@ProductID"].Value = strProdID;
+                cmdSQL.CommandText = "uspGetProductList";
 
                 cmdSQL.Parameters.Add(new SqlParameter("@ErrCode", SqlDbType.Int));
-                cmdSQL.Parameters["ErrCode"].Direction = ParameterDirection.ReturnValue;
+                cmdSQL.Parameters["@ErrCode"].Direction = ParameterDirection.ReturnValue;
 
                 dsSQL = new DataSet();
                 try
@@ -94,15 +147,70 @@ namespace CCIS2645_Project2_ErinKinnen
                     cnSQL.Dispose();
                 }
             }
+
+            if (blnErrorOccurred)
+            {
+                return null;
+            }
+            else
+            {
+                return dsSQL;
+            }
         }
 
-        if(blnErrorOccurred)
+        public static DataSet GetProducts()
         {
-            return null;
-        }
-        else
-        {
-            return dsSQL;
+            SqlConnection cnSQL;
+            SqlCommand cmdSQL;
+            SqlDataAdapter daSQL;
+            DataSet dsSQL = null;
+            Boolean blnErrorOccurred = false;
+            Int32 intRetCode;
+
+            cnSQL = AcquireConnection();
+            if(cnSQL == null)
+            {
+                blnErrorOccurred = true;
+            }
+            else
+            {
+                cmdSQL = new SqlCommand();
+                cmdSQL.Connection = cnSQL;
+                cmdSQL.CommandType = CommandType.StoredProcedure;
+                cmdSQL.CommandText = "uspGetProducts";
+
+                cmdSQL.Parameters.Add(new SqlParameter("@ErrCode", SqlDbType.Int));
+                cmdSQL.Parameters["@ErrCode"].Direction = ParameterDirection.ReturnValue;
+
+                dsSQL = new DataSet();
+                try
+                {
+                    daSQL = new SqlDataAdapter(cmdSQL);
+                    intRetCode = daSQL.Fill(dsSQL);
+                    daSQL.Dispose();
+                }
+                catch (Exception ex)
+                {
+                    blnErrorOccurred = true;
+                    dsSQL.Dispose();
+                }
+                finally
+                {
+                    cmdSQL.Parameters.Clear();
+                    cmdSQL.Dispose();
+                    cnSQL.Close();
+                    cnSQL.Dispose();
+                }
+            }
+
+            if (blnErrorOccurred)
+            {
+                return null;
+            }
+            else
+            {
+                return dsSQL;
+            }
         }
     }
 
